351b41a6e1e1f1978b8286e2a34aa48b043bc977
mufflon <alexander@wallindevelopment.se>
2017-10-31 21:59:16 +0100

    initial information

---
+++ src/com/contribe/arbetsprov/structure/Book.java
@@ -0,0 +1,13 @@
+package com.contribe.arbetsprov.structure;
+
+import java.math.BigDecimal;
+
+public class Book {
+
+	private String title;
+
+	private String author;
+
+	private BigDecimal price;
+
+}
