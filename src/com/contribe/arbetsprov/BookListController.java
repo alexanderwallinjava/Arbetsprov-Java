d63493e2a3536b24bb79229ac70f90bef28f671f
mufflon <alexander@wallindevelopment.se>
2017-11-02 18:42:24 +0100

    mindre semantisk ändring booklistcontroller

--- src/com/contribe/arbetsprov/BookListController.java
+++ src/com/contribe/arbetsprov/BookListController.java
@@ -1,14 +1,10 @@
 package com.contribe.arbetsprov;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
