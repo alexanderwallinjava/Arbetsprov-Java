d66d9980a2aa5b5457aff644c2d233a237b2c137
mufflon <alexander@wallindevelopment.se>
2017-11-02 09:56:12 +0100

    proper logging in the synchronizedbooklist; added logger to booklistcontroller

--- src/com/contribe/arbetsprov/BookListController.java
+++ src/com/contribe/arbetsprov/BookListController.java
@@ -1,5 +1,7 @@
 package com.contribe.arbetsprov;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookListController {
 
+	Logger logger = LoggerFactory.getLogger(BookListController.class);
+	
 	@Autowired
 	SynchronizedBookList books;
 	
--- src/com/contribe/arbetsprov/SynchronizedBookList.java
+++ src/com/contribe/arbetsprov/SynchronizedBookList.java
@@ -1,7 +1,6 @@
 package com.contribe.arbetsprov;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 import java.util.TreeMap;
@@ -9,6 +8,8 @@ import java.util.TreeSet;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class SynchronizedBookList implements BookList {
 
+	Logger logger = LoggerFactory.getLogger(SynchronizedBookList.class);
+	
 	@Autowired
 	Environment env;
 	
@@ -72,29 +75,42 @@ public class SynchronizedBookList implements BookList {
 	}
 	
 	@PostConstruct
-	public void init() throws IOException {
-		System.out.println("initializing SynchronizedBookList");
-		
-		URL url = new URL(env.getProperty("arbetsprov.url"));
-		
-		Scanner in = new Scanner(url.openStream(), "UTF-8");
-		
-		while(in.hasNextLine()) {
-			String line = in.nextLine();
-			String[] elems = line.trim().split(";");
-			System.out.println(line);
-			if(elems.length == 4) {
-				Book book = new Book(elems[0], elems[1], elems[2].replaceAll(",", ""));
-				this.add(book, Integer.parseInt(elems[3]));
+	public synchronized void init() {
+		try {
+			logger.info("populating SynchronizedBookList with data from url: " + env.getProperty("arbetsprov.url"));
+			
+			URL url = new URL(env.getProperty("arbetsprov.url"));
+			
+			Scanner in = new Scanner(url.openStream(), "UTF-8");
+			
+			while(in.hasNextLine()) {
+				String line = in.nextLine();
+				String[] elems = line.trim().split(";");
 				
-				System.out.println("added book");
-			} else {
-				//logga
+				//stripping commas from numbers formatted like 100,000,000
+				elems[2] = elems[2].replaceAll(",", "");
+
+				if(elems.length == 4) {
+					logger.debug("adding book: "  + line);
+					
+					try {
+						Book book = new Book(elems[0], elems[1], elems[2]);
+						
+						this.add(book, Integer.parseInt(elems[3]));
+					} catch(NumberFormatException E) {
+						logger.error("number formatting error when parsing bookstoredata from url; possible error sources: {" + elems[2] + "," + elems[3] + "}");
+					}
+				} else {
+					logger.warn("source format incompatible at Bookstoredata, book not added: " + line);
+				}
+				
 			}
+			logger.info("BookList populated with Bookstoredata from url");
+			in.close();
+		} catch(IOException E) {
+			logger.error("unable to populate BookList with data from url: " + E.toString());
 		}
-		in.close();
 		
-		System.out.println(env.getProperty("arbetsprov.url"));
 	}
 
 }
